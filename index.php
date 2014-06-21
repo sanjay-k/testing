780511775d777da9264eadd0fb2478809e57534e
sanjay.k <sanjay.k@webreinvent.com>
2014-06-21 15:28:24 +0530

    testing commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+
+<?php get_header(); ?>
+<body>
+
+
+  
+
+    <!--bottom Slider-->
+    
+    <div class="main_con bg_whtf5">
+    <div class="container">
+    <div class="proj_slider">
+    <h1>Our Projects.</h1>
+    </div>
+    </div>
+   
+    <div id="logoParade">
+   
+   
+		<a href="#"><img src="<?php echo bloginfo('template_url');?>/img/btm_slider1.jpg" alt="playing"><div>Rice<span>view project >> </span></div></a>
+        
+      	
+      	<div>
+        <a href="#"><img src="<?php echo bloginfo('template_url');?>/img/btm_slider2.jpg" alt="health care"><div>Health Care <span>view project >> </span></div></a>
+        <br/>
+       	<a href="#"  class="btmchild"><img src="<?php echo bloginfo('template_url');?>/img/btm_slider3.jpg" alt=""><div>animal<span>view project >> </span></div></a>
+        </div>
+		<a href="#"><img src="<?php echo bloginfo('template_url');?>/img/btm_slider4.jpg" alt=""><div>save water<span>view project >> </span></div></a>
+        <div>
+		<a href="#"><img src="<?php echo bloginfo('template_url');?>/img/btm_slider5.jpg" alt=""><div>places<span>view project >> </span></div></a> <br/>
+		<a href="#" class="btmchild"><img src="<?php echo bloginfo('template_url');?>/img/btm_slider6.jpg" alt=""><div>education<span>view project >> </span></div></a>
+        </div>
+		<a href="#"><img src="<?php echo bloginfo('template_url');?>/img/btm_slider7.jpg" alt=""><div>planting<span>view project >> </span></div></a>
+	</div>
+  
+    </div>
+    <!--/bottom Slider-->
+    <?php get_footer(); ?>
+    
+    
+    
+
+    
+</body>
+</html>
